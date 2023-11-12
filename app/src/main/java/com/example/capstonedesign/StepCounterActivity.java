c0d4ccfcb43403b9d0e527f382edbea567d23738
a0100019 <dldbqls0019@naver.com>
2023-11-12 22:07:05 +0900

    걸음 수 그래프 추가

--- app/src/main/java/com/example/capstonedesign/StepCounterActivity.java
+++ app/src/main/java/com/example/capstonedesign/StepCounterActivity.java
@@ -8,16 +8,34 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class StepCounterActivity extends AppCompatActivity {
     private TextView tvSteps;
     private StepReceiver stepReceiver;
+    ArrayList<Integer> jsonList = new ArrayList<>(); // ArrayList 선언
+    ArrayList<String> labelList = new ArrayList<>(); // ArrayList 선언
+    BarChart barChart;
+    TextView minuteTextview;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,18 +44,18 @@ public class StepCounterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_step_counter);
 
         tvSteps = findViewById(R.id.tvSteps);
-        Button btnStartService = findViewById(R.id.btnStartService);
         Button btnStopService = findViewById(R.id.btnStopService);
         stepReceiver = new StepReceiver();
 
-        btnStartService.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    startStepCounterService();
-                }
-            }
-        });
+        SharedPreferences preferences = getSharedPreferences("user_preferences", MODE_PRIVATE);
+        String uid = preferences.getString("uid", "");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Users").document(uid).collection("message");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startStepCounterService();
+        }
+
 
         btnStopService.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +63,14 @@ public class StepCounterActivity extends AppCompatActivity {
                 stopStepCounterService();
             }
         });
+
+        barChart = (BarChart) findViewById(R.id.fragment_bluetooth_chat_barchart);
+        graphInitSetting();       //그래프 기본 세팅
+
+        BarChartGraph(labelList, jsonList);
+        barChart.setTouchEnabled(false); //확대하지못하게 막아버림! 별로 안좋은 기능인 것 같아~
+//        barChart.getAxisRight().setAxisMaxValue(500);
+//        barChart.getAxisLeft().setAxisMaxValue(500);
     }
 
     @Override
@@ -76,8 +102,65 @@ public class StepCounterActivity extends AppCompatActivity {
     }
 
     private void stopStepCounterService() {
-        Intent serviceIntent = new Intent(this, StepCounterService.class);
-        stopService(serviceIntent);
-        Log.d("StepCounterActivity", "Service stopped");
+
     }
+
+    public void graphInitSetting(){
+
+        labelList.add("12일");
+        labelList.add("월");
+        labelList.add("화");
+        labelList.add("수");
+        labelList.add("목");
+        labelList.add("금");
+        labelList.add("토");
+
+        jsonList.add(30);
+        jsonList.add(20);
+        jsonList.add(2000);
+        jsonList.add(400);
+        jsonList.add(50);
+        jsonList.add(60);
+        jsonList.add(70);
+
+
+        BarChartGraph(labelList, jsonList);
+        barChart.setTouchEnabled(false); //확대하지못하게 막아버림! 별로 안좋은 기능인 것 같아~
+        //barChart.setRendererLeftYAxis();
+//        barChart.setMaxVisibleValueCount(50);
+//        barChart.setTop(50);
+//        barChart.setBottom(0);
+        barChart.setAutoScaleMinMaxEnabled(true);
+//        barChart.getAxisRight().setAxisMaxValue(80);
+//        barChart.getAxisLeft().setAxisMaxValue(500);
+    }
+    /**
+     * 그래프함수
+     */
+    private void BarChartGraph(ArrayList<String> labelList, ArrayList<Integer> valList) {
+        // BarChart 메소드
+
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        for (int i = 0; i < valList.size(); i++) {
+            entries.add(new BarEntry((Integer) valList.get(i), i));
+        }
+
+        BarDataSet depenses = new BarDataSet(entries, "일일 걸음 수"); // 변수로 받아서 넣어줘도 됨
+        depenses.setAxisDependency(YAxis.AxisDependency.LEFT);
+        barChart.setDescription(" ");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        for (int i = 0; i < labelList.size(); i++) {
+            labels.add((String) labelList.get(i));
+        }
+
+        BarData data = new BarData(labels, depenses); // 라이브러리 v3.x 사용하면 에러 발생함
+        depenses.setColors(ColorTemplate.LIBERTY_COLORS); //
+
+        barChart.setData(data);
+        barChart.animateXY(1000, 1000);
+        barChart.invalidate();
+    }
+
 }
